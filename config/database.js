fe5a41f32c4baed7840e4db0ede4715ddb2dff8d
BÁRBARA COELHO RIOS <barbara_rios@estudante.sesisenai.org.br>
2024-10-15 21:51:29 -0300

    aaa

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 // Configuração da conexão com o CockroachDB
-const sequelize = new Sequelize('postgresql://barbara:o6oq-5psSjRTZdeLmRn9TQ@senai-api-1691.jxf.gcp-southamerica-east1.cockroachlabs.cloud:26257/defaultdb', {
+const sequelize = new Sequelize('postgresql:', {
   dialect: 'postgres', // Dialeto para o CockroachDB é 'postgres'
   protocol: 'postgres',
   dialectOptions: {
